fc90b7a33c5c5ba3a8aa09689afd60b38a196d06
santu <j6458221@yahoo.com.tw>
2024-01-04 11:15:21 +0800

    Updates

---
+++ assets/login-371ca16e.js
@@ -0,0 +1,1 @@
+import"./main-aa959d62.js";const t=document.querySelector("#login_btn");t.addEventListener("click",()=>{window.location.assign("https://santuhat.github.io/spring-festival-lottery/")});
